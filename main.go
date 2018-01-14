d2e77fdb4c92b866520fb9b5fa907bc00b29f00c
Florian Schrag <florian@schrag.org>
2018-01-14 19:49:24 +0100

    hello with negroni

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/urfave/negroni"
+	"log"
+	"os"
+)
+
+var logger *log.Logger
 
 func main() {
-	fmt.Println(hello())
+	logger := log.New(os.Stdout, "[force]   ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, hello())
+	})
+
+	n := negroni.Classic() // Includes some default middlewares
+	n.UseHandler(mux)
+
+	listenOn := ":3000"
+
+	logger.Printf("will listen on %s", listenOn)
+	http.ListenAndServe(listenOn, n)
 }
 
 func hello() string {
