b335f31b5f0f54371e3d95d4261572fee8cfe62e
Florian Schrag <florian@schrag.org>
2018-01-26 07:41:46 +0100

    embed documentation and give an error if we fail on startup

--- main.go
+++ main.go
@@ -6,9 +6,19 @@ import (
 
 	"github.com/gitu/force/goraml"
 
+	"bytes"
 	"github.com/gorilla/mux"
 	"gopkg.in/validator.v2"
+	"io"
 )
+
+//go:generate go-bindata-assetfs -pkg main apidocs/... index.html
+
+func serveIndex(rw http.ResponseWriter, req *http.Request) {
+	bs, _ := Asset("index.html")
+	var reader = bytes.NewBuffer(bs)
+	io.Copy(rw, reader)
+}
 
 func main() {
 	// input validator
@@ -17,17 +27,21 @@ func main() {
 	r := mux.NewRouter()
 
 	// home page
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
+	r.HandleFunc("/", serveIndex)
+	r.PathPrefix("/client/").Handler(http.StripPrefix("/client/", http.FileServer(http.Dir("./client/"))))
 
 	// apidocs
-	r.PathPrefix("/apidocs/").Handler(http.StripPrefix("/apidocs/", http.FileServer(http.Dir("./apidocs/"))))
+	r.PathPrefix("/apidocs/").Handler(http.StripPrefix("/apidocs/", http.FileServer(assetFS())))
 
 	EntriesInterfaceRoutes(r, EntriesAPI{})
 
 	GroupsInterfaceRoutes(r, GroupsAPI{})
 
-	log.Println("starting server")
-	http.ListenAndServe(":5000", r)
+	log.Println("starting server visit http://localhost:5000 to access it")
+	log.Println("   if you want to serve your own files put them in the folder ./client")
+	log.Println("   then visit to http://localhost:5000/client/")
+	err := http.ListenAndServe(":5000", r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
